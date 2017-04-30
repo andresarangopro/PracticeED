f96adcd73460ffa9a47d1ba8d14f9a7a614f216d
Sebassllr <sebaslunasllr@gmail.com>
2017-04-30 09:53:08 -0500

    Contenido del árbol, visualización del árbol

---
+++ Trabajo2/src/operaciones/TreePrinter.java
@@ -0,0 +1,22 @@
+package operaciones;
+/**
+ * Clase que imprime el arbol binario
+ * @author Sebastian Luna R
+ *
+ */
+public class TreePrinter {
+	
+	private Tree tree;
+	
+	public TreePrinter(Tree arbol) {
+		tree = arbol;
+	}	
+	
+	private void imprimir(Tree arbol){
+		
+		
+		
+	}
+	
+	
+}
